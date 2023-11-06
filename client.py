c96cf4e9b62945d069ba80b3d3a65d42ff7ed295
ZachSchwartz <zschwart@iwu.edu>
2023-11-06 11:22:33 -0600

    update

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
 import socket
 #client
 
-HOST = "10.6.9.7"  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    user_input = "What would you like to say?"
+    user_input = input("What would you like to say?")
     s.sendall(user_input.encode('utf-8'))
-    s.sendall(b"\n")
+    s.sendall("\n")
     if user_input == "":
-        s.sendall(b"\n")
+        s.sendall("\n")
     data = s.recv(1024) # when recieving must specify how many bytes to recieve
 
 print(f"Received {data!r}")
