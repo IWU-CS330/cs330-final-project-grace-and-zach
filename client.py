774b39c4a372f75762695b1520b183ddd886556b
ZachSchwartz <zschwart@iwu.edu>
2023-11-06 18:23:57 -0600

    name

--- client.py
+++ client.py
@@ -6,11 +6,12 @@ PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    user_input = input("What would you like to say?")
-    s.sendall(user_input.encode('utf-8'))
+    while True:
+        user_name = input("What is your name")
+        if user_name != "":
+            break
+    s.sendall(user_name.encode('utf-8'))
     s.sendall(b"\n")
-    if user_input == "":
-        s.sendall(b"\n")
     data = s.recv(1024) # when recieving must specify how many bytes to recieve
     data = data.decode("utf-8")
 
