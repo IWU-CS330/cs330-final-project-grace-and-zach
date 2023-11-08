82a3190b7493b13f9626c447a9dc54c10fdfbfed
Grace Poulton <gpoulton@iwu.edu>
2023-11-08 08:32:08 -0600

    Names Table created, along with beginnings of sql queries for week2

--- client.py
+++ client.py
@@ -6,14 +6,17 @@ PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    #user_name = input("What is your name:")
     while True:
-        user_name = input("What is your name")
-        if user_name != "":
+        user_name = input("What is your name:")
+        if user_name == "":
             break
-    s.sendall(user_name.encode('utf-8'))
-    s.sendall(b"\n")
-    data = s.recv(1024) # when recieving must specify how many bytes to recieve
-    data = data.decode("utf-8")
+        s.sendall(user_name.encode('utf-8'))
+        s.sendall(b"\n")
+        data = s.recv(1024) # when recieving must specify how many bytes to recieve
+        data = data.decode("utf-8")
 
 
-print(f"Received {data!r}")
+        print(f"Received {data!r}")
+        #break
+    # print here a list of commands or something like that (keep them in the loop until they want to leave the program)
---
+++ names.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE database;
+
+CREATE TABLE names (
+    ID int,
+    username varchar(80),
+    PRIMARY KEY (ID)
+)
+
+SELECT * from names;
