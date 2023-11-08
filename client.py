87c49020d5a70a19a294c3f3e75eb2ce75576964
ZachSchwartz <zschwart@iwu.edu>
2023-11-08 11:51:52 -0600

    runs in a loop

--- client.py
+++ client.py
@@ -7,13 +7,17 @@ PORT = 65432  # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
-        user_name = input("What is your name")
+        user_name = input("What is your name\n")
         if user_name != "":
             break
     s.sendall(user_name.encode('utf-8'))
     s.sendall(b"\n")
     data = s.recv(1024) # when recieving must specify how many bytes to recieve
     data = data.decode("utf-8")
-
-
-print(f"Received {data!r}")
+    while True:
+        user_input = input("enter a message\n")
+        s.sendall(user_name.encode('utf-8'))
+        s.sendall(b"\n")
+        data = s.recv(1024) # when recieving must specify how many bytes to recieve
+        data = data.decode("utf-8")
+    print(f"Received {data!r}")
