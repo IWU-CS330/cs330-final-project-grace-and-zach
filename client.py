263cdf9b7a0066d37f918efacfd97614645fca43
ZachSchwartz <zschwart@iwu.edu>
2023-11-08 21:38:46 -0600

    updates

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import client_class
 #client
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
@@ -10,14 +11,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         user_name = input("What is your name\n")
         if user_name != "":
             break
-    s.sendall(user_name.encode('utf-8'))
+    s.sendall(client_class.set_username(user_name))
     s.sendall(b"\n")
+    s.sendall(client_class.names())
     data = s.recv(1024) # when recieving must specify how many bytes to recieve
     data = data.decode("utf-8")
+    print(client_class.help())
     while True:
         user_input = input("enter a message\n")
         if(user_input == "close"):
+            s.sendall(client_class.close_connection())
             break
+        if(user_input == "names"):
+            s.sendall(client_class.names())
+        if(user_input == "help"):
+            print(client_class.help())
         s.sendall(user_name.encode('utf-8'))
         s.sendall(b"\n")
         data = s.recv(1024) # when recieving must specify how many bytes to recieve
