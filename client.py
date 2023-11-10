5e4eca5ced9ad72355a827cf859508e28551bb27
Grace Poulton <gpoulton@iwu.edu>
2023-11-09 23:56:15 -0600

    Added a Git Ignore and got rid of names.db file, and made the names and adding new person function, functionable (for at least one person)

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import socket
-import sqlite3
 #client
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
@@ -7,17 +6,31 @@ PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    user_name = input("What is your name:")
     while True:
-        #user_name = input("What is your name:")
-        if user_name == "":
+        user_name = input("What is your name\n")
+        if user_name != "":
             break
-        s.sendall(user_name.encode('utf-8'))
-        s.sendall(b"\n")
-        data = s.recv(100000000) # when recieving must specify how many bytes to recieve
-        data = data.decode("utf-8")
 
+    s.sendall(user_name.encode('utf-8'))
+    #s.sendall(b"\n")
+   
+    data = s.recv(1024) # when recieving must specify how many bytes to recieve
+    data = data.decode("utf-8")
+    print(data)
 
-        print(f"Received {data!r}")
-        break
-        # print here a list of commands or something like that (keep them in the loop until they want to leave the program)
+    while True:
+        user_input = input("enter a message\n")
+        if(user_input == "close"):
+            break
+        s.sendall(user_input.encode('utf-8'))
+        #s.sendall(b"\n")
+        #code block for allowing mutliple names in names function
+        name_list = []  
+        while data != 'stop':
+            data = s.recv(1024) # when recieving must specify how many bytes to recieve
+            data = data.decode("utf-8")
+            name_list = name_list.append(data)
+
+        print("Here is a list of all of our current users:", name_list)
+
+    s.close()
