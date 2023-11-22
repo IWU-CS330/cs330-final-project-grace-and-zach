a08b3774cefbf5b492c4da87e7bc3c5f40419e6e
ZachSchwartz <zschwart@iwu.edu>
2023-11-21 23:54:19 -0600

    Update client_class.py

--- client_class.py
+++ client_class.py
@@ -1,10 +1,13 @@
 #class clientclass:
+#def find_command(input, message, username):
+
+
 def set_username(user_name):
     user_name = "  set_username  " + user_name
-    user_name = str(len(user_name)) + ' ' + user_name
+    user_name = str(len(user_name)) + user_name
     return user_name.encode('utf-8')
 
-def names():
+def list_names():
     return "7  names".encode('utf-8')
 
 def help():
@@ -16,8 +19,31 @@ def help():
 
 def message(message, username):
     message = "  message  " + username + ": " + message
-    message = str(len(message)) + ' ' + message
+    message = str(len(message)) + message
     return message.encode('utf-8')
+
+def create_room(room_name, username):
+    message = "  create  " + room_name + ' ' + username
+    message = str(len(message)) + message
+    return message.encode('utf-8')
+
+def add_user(message, username):
+    message = "  add  " + username + message
+    message = str(len(message)) + message
+    return message.encode('utf-8')
+
+def leave_room(username):
+    message = "  leave  " + username
+    message = str(len(message)) + message
+    return message.encode('utf-8')
+
+def join_room(message, username):
+    message = "  join  " + username + ' ' + message
+    message = str(len(message)) + message
+    return message.encode('utf-8')
+
+def list_rooms():
+    return "7  rooms".encode('utf-8')
 
 def close_connection():
     return "7  close".encode('utf-8')
