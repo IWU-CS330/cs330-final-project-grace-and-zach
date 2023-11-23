4c94dc8b9fd4291826b2c2ff7ba0fb2fe51a685c
ZachSchwartz <zschwart@iwu.edu>
2023-11-23 14:43:40 -0600

    reworking of clientside

--- client.py
+++ client.py
@@ -1,45 +1,34 @@
+#client
 import socket
 import client_class
-#client
+import threading
+
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+def receive_messages(socket):
+    while True:
+        data = socket.recv(1024)
+        if not data:
+            break
+        print(f"{data.decode('utf-8')}")
+
+def client_startup(socket):
+    client_class.set_socket(socket)
     while True:
         username = input("What is your name\n")
         if username != "":
             break
     s.sendall(client_class.set_username(username))
-    #s.sendall(b"\n")
-    
-    data = s.recv(1024) # when recieving must specify how many bytes to recieve
-    print(data.decode("utf-8"))
-    s.sendall(client_class.list_names())
-    data = s.recv(1024)
-    data = data.decode("utf-8")
-    print("Here is a list of usernames from our current users:", data)
-    print(client_class.help())
+    client_class.help()
     while True:
         user_input = input("enter a message\n")
         split_input = user_input.split()
-        client_class.find_command(split_input[1], split_input[2], username)
-        if(user_input == "close"):
-            s.sendall(client_class.close_connection())
-            print("Connection Closed")
-            break
-        elif(user_input == "names"):
-            s.sendall(client_class.names())
-            data = s.recv(1024)
-            data = data.decode("utf-8")
-            print("Here is a list of usernames from our current users:", data)
-        elif(user_input == "help"):
-            print(client_class.help())
-        else:
-            s.sendall(client_class.message(user_input, username))
-            s.sendall(b"\n")
-            data = s.recv(1024) # when recieving must specify how many bytes to recieve
-            data = data.decode("utf-8")
+        client_class.find_command(split_input[1], split_input[2])
 
-    s.close()
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        send_thread = threading.Thread(target=client_startup, args=(s,))
+        receive_thread = threading.Thread(target=receive_messages, args=(s,))
--- client_class.py
+++ client_class.py
@@ -1,49 +1,91 @@
-#class clientclass:
-#def find_command(input, message, username):
+import socket
 
+class clientclass:
+    def __init__(self):
+            self.self.username = None
+            self.socket = None
+            self.room = False
 
-def set_username(user_name):
-    user_name = "  set_username  " + user_name
-    user_name = str(len(user_name)) + user_name
-    return user_name.encode('utf-8')
+    def set_socket(self, socket):
+        self.socket = socket
 
-def list_names():
-    return "7  names".encode('utf-8')
+    def set_username(self, username):
+        self.username = username
+        self.username = "  set_username  " + self.username
+        self.username = str(len(self.username)) + self.username
+        self.socket.sendall(self.username.encode('utf-8')) 
 
-def help():
-    return """Current commands available:
-    set_name: sets username
-    names: returns list of all users
-    close: closes connection
-    help: lists all commands"""
+    def list_names(self):
+        self.socket.sendall("7  names".encode('utf-8'))
 
-def message(message, username):
-    message = "  message  " + username + ": " + message
-    message = str(len(message)) + message
-    return message.encode('utf-8')
+    def help(self):
+        print("""Current commands available:
+        names: returns list of all users
+        rooms: lists rooms
+        join: joins chatroom
+        create: creates a chatroom
+        add: adds user to your room
+        leave: leaves room you are in
+        close: closes connection
+        help: lists all commands""")
 
-def create_room(room_name, username):
-    message = "  create  " + room_name + ' ' + username
-    message = str(len(message)) + message
-    return message.encode('utf-8')
+    def message(self, message):
+        message = "  message  " + self.username + ": " + message
+        message = str(len(message)) + message
+        self.socket.sendall(message.encode('utf-8'))
 
-def add_user(message, username):
-    message = "  add  " + username + message
-    message = str(len(message)) + message
-    return message.encode('utf-8')
+    def create_room(self, room_name):
+        self.room = True
+        message = "  create  " + self.username + ' ' + room_name
+        message = str(len(message)) + message
+        self.socket.sendall(message.encode('utf-8'))
 
-def leave_room(username):
-    message = "  leave  " + username
-    message = str(len(message)) + message
-    return message.encode('utf-8')
+    def add_user(self, message):
+        message = "  add  " + self.username + message
+        message = str(len(message)) + message
+        self.socket.sendall(message.encode('utf-8'))
 
-def join_room(message, username):
-    message = "  join  " + username + ' ' + message
-    message = str(len(message)) + message
-    return message.encode('utf-8')
+    def leave_room(self):
+        self.room = False
+        message = "  leave  " + self.username
+        message = str(len(message)) + message
+        self.socket.sendall(message.encode('utf-8'))
 
-def list_rooms():
-    return "7  rooms".encode('utf-8')
+    def join_room(self, message):
+        self.room = True
+        message = "  join  " + self.username + ' ' + message
+        message = str(len(message)) + message
+        self.socket.sendall(message.encode('utf-8'))
 
-def close_connection():
-    return "7  close".encode('utf-8')
+    def list_rooms(self):
+        self.socket.sendall("7  rooms".encode('utf-8'))
+
+    def close_connection(self):
+        self.socket.sendall("7  close".encode('utf-8'))
+    
+    def find_command(self, input, message):
+        #Could add reset name method
+        if input == 'names':
+            clientclass.list_names()
+        elif input == 'help':
+            clientclass.help()
+        elif input == 'create':
+            clientclass.create_room(message)
+        elif input == 'add':
+            clientclass.add_user(message)
+        elif input == 'join':
+            clientclass.join_room(message)
+        elif input == 'rooms':
+            clientclass.list_rooms()
+        elif input == 'close':
+            clientclass.close_connection()
+        else:
+            if self.room == True:
+                if input == 'leave':
+                    clientclass.leave_room(message)
+                else:
+                    clientclass.message(input + message)
+            else:
+                print("Sorry, we didn't understand that command")
+
+    
