d60aebdd02f46905b3ac941b47a87d6ea50de3ea
poultong <113626402+poultong@users.noreply.github.com>
2023-12-13 18:00:33 -0600

    Update client_class.py

--- client_class.py
+++ client_class.py
@@ -1,63 +1,68 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
+import asyncio
+import os
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives import serialization, hashes
+# from cryptography.hazmat.primitives.asymmetric import rsa, padding
 
 class ClientClass:
     def __init__(self):
             self.username = None
             self.socket = None
             self.room = False  
-            self.private_key = None
-            self.public_keys = []
+            # self.private_key = None
+            # self.public_key = None
 
     def set_username_socket(self, username, socket):
         self.socket = socket
         self.username = username
-        message = "  set_username  " + username + '\n'
+        message = "  set_username  " + username + "\n"
         message = str(len(message)) + message 
         socket.sendall(message.encode('utf-8'))
-        self.set_key()
+        # self.set_key()
 
-    def set_key(self):
-        self.private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-            backend=default_backend()
-        )
-        message =  " set_public_key " + self.username + str(self.private_key.public_key())
-        message = str(len(message)) + message 
-        self.socket.sendall(message.encode('utf-8'))
+    # def set_key(self):
+    #     self.private_key = rsa.generate_private_key(
+    #         public_exponent=65537,
+    #         key_size=2048,
+    #         backend=default_backend()
+    #     )
+    #     self.public_key = self.private_key.public_key()
+    #     message =  " public_key " + self.public_key
+    #     self.socket.sendall(len(message) + message.encode('utf-8'))
+
+    def list_names(self):
+        self.socket.sendall("7  names".encode('utf-8'))
+
+    def list_members(self):
+        room_name = input("What room would you like to check the members of?")
+        length = len("namesof " + room_name)
+        self.socket.sendall((str(length + 1) + " namesof " + room_name).encode('utf-8'))
 
     def help(self):
         print("""Current commands available:
         names: returns list of all users
-        reset: resets username
-        create: creates a chatroom
         rooms: lists rooms
         join: joins chatroom
-        members: lists members of room
-        file: sends a file
+        create: creates a chatroom
         leave: leaves room you are in
         close: closes connection
+        send: sends a file
+        members: lists members of room
         help: lists all commands""")
 
-    def list_names(self):
-        self.socket.sendall("7 names \n".encode('utf-8'))
-    
-    def list_members(self):
-        self.socket.sendall("7 namesof \n".encode('utf-8'))
 
-    def reset_name(self):
-        name = input("What would you like your new name to be?\n")
-        message = "  reset_name  " + name + "\n"
-        message = str(len(message)) + message
+    def message(self, message):
+        message = "  message  " + self.username + " " + message
+        message = str(len(message)) + message + "\n"
         self.socket.sendall(message.encode('utf-8'))
+
 
     def create_room(self):
         room_name = input("What would you like your room name to be?\n")
         message = "  create  " + self.username + ' ' + room_name
         message = str(len(message)) + message + "\n"
         self.socket.sendall(message.encode('utf-8'))
-        print("Created room: " + room_name)
+        print("created room: " + room_name)
 
     def leave_room(self):
         self.room = False
@@ -75,49 +80,26 @@ class ClientClass:
         print("Joined room: " + room_name)
 
     def list_rooms(self):
-        self.socket.sendall("7  rooms \n".encode('utf-8'))
+        self.socket.sendall("7  rooms".encode('utf-8'))
 
     def close_connection(self):
-        self.socket.sendall("7  close \n".encode('utf-8'))
-    
-    def message(self, message):
-        self.socket.sendall("16 get_public_keys".encode('utf-8'))
-        for key in self.public_keys:
-            message = key.encrypt(
-                message,
-                padding.PKCS1v15()
-            )
-        message = "  message  " + self.username + " " + message
-        message = str(len(message)) + message + "\n"
-        self.socket.sendall(message.encode('utf-8'))
+        self.socket.sendall("7  close".encode('utf-8'))
 
     def send_file(self):
         file_path = input("What is the path to the file you'd like to send?\n")
         file_name = input("What is the name of the file you'd like to send?\n")
         with open(file_path, 'rb') as file:
             file_data = file.read()
-        self.socket.sendall("16 get_public_keys".encode('utf-8'))
-        for key in self.public_keys:
-            message = key.encrypt(
-                file_data,
-                padding.PKCS1v15()
-            )
-            header = " file " + self.username + " " + file_name
-            self.socket.sendall(len(header) + header)
-            self.socket.sendall(len(message) + message)
-    
-    def decrypt_message(self, input):
-        decrypted_message = self.private_key.decrypt(
-            input,
-            padding.PKCS1v15()
-        )
-        return decrypted_message
 
+        header = "  file  " + file_name + " " + self.username + " "
+        self.socket.sendall((str(len(file_data) + len(header))).encode("utf-8"))
+        self.socket.sendall(header.encode("utf-8"))
+        self.socket.sendall(file_data)
+    
     def find_command(self, input):
+        #Could add reset name method
         if input == 'names':
             self.list_names()
-        elif input == 'reset':
-            self.reset_name()
         elif input == 'help':
             self.help()
         elif input == 'create':
@@ -132,11 +114,13 @@ class ClientClass:
             if self.room == True:
                 if input == 'leave':
                     self.leave_room()
-                elif input == 'members':
-                    self.list_members()
                 elif input == 'send':
                     self.send_file()
+                elif input == 'members':
+                    self.list_members()
                 else:
                     self.message(input)
             else:
                 print("Sorry, we didn't understand that command")
+
+    
